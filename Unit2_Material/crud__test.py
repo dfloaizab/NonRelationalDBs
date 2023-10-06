317981bccb7851d1320f416ccd3dd41d8dd3dac0
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-10-06 16:36:43 -0500

    Create crud__test.py

---
+++ Unit2_Material/crud__test.py
@@ -0,0 +1,37 @@
+import couchdb
+#conexión a la BD:
+
+user = "admin"
+pwd = "admin"
+host = "127.0.0.1"
+port = "5984"
+
+#couch_server  = couchdb.Server("http://admin:admin@127.0.0.1:5984/")
+couch_server = couchdb.Server(f"http://{user}:{pwd}@{host}:{port}/")
+
+db_name = "refugio_animales"
+
+#ejemplo de creación de un store que no aún no existe
+#couch_server.create(db_name)
+
+#selección de un store existente:
+db = couch_server[db_name]
+
+#Operaciones del crud:
+# (1) Insert: creación de un documento
+#información de dos refugios:
+db.save({"_id":"1","nit":"98989898","rep_legal":"Diego L","fondos":0.0})
+db.save({"_id":"2","nit":"50505050","rep_legal":"Diana L","fondos":0.0})
+
+# (2)  Select: selección de un documento por un determinado valor de llave ("_id")
+doc_creado = db["1"]
+print(doc_creado)
+
+# (3) Update: Modificación de un documento previamente consultado:
+doc_creado["fondos"] = 1000000.0
+db.save(doc_creado)
+print(doc_creado)
+
+# (4) Delete:Borrado de un documento existente
+doc_borrar = db["2"]
+db.delete(doc_borrar)
